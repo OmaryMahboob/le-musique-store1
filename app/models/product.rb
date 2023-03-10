9519cc8f95ff184b906114f183e16cd0246249b4
mahboob omary <mahboob.omary@gmail.com>
2023-03-10 12:59:16 +0100

    the products contorller and its actions are completed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :address, presence:true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root to: "products#index"
+  devise_for :users
   resources :products do
     # resources :reviews, only: [ :new, :create ]
   end
   # resources :reviews, only: [:edit, :destroy, :update]
 
-
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
