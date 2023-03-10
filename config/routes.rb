8c80520886e406d09ee835fabaaa5debbe4ed042
mahboob omary <mahboob.omary@gmail.com>
2023-03-10 12:38:39 +0100

    products controller has the 7 routes and functions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root to: "products#index"
+  resources :products do
+    # resources :reviews, only: [ :new, :create ]
+  end
+  # resources :reviews, only: [:edit, :destroy, :update]
+
+
   devise_for :users
-  root to: "pages#home"
-  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
