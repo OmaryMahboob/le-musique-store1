0b58bba68d365b537e03ceb8b0d07a67af86fa5e
mahboob omary <mahboob.omary@gmail.com>
2023-03-13 11:55:45 +0100

    just for a new pull"

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: "products#index"
   devise_for :users
   resources :products do
-    # resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [ :new, :create ]
   end
-  # resources :reviews, only: [:edit, :destroy, :update]
+  resources :reviews, only: [:edit, :destroy, :update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
