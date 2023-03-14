f844695283eaf89bdc363f3ec02d18e92592854d
jane eme <janeeme23@gmail.com>
2023-03-14 10:03:02 +0100

    saving work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,13 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+
+    @markers = @products.geocoded.map do |product|
+      {
+        lat: product.latitude,
+        lng: product.longitude
+      }
+    end
   end
 
   def show
