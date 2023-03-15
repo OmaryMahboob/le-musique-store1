0a8253bf037bae151219ec8e74e872db5f188082
jane eme <janeeme23@gmail.com>
2023-03-15 09:55:43 +0100

    geocoding done

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="address-autocomplete"
+export default class extends Controller {
+  connect() {
+  }
+  static values = { apiKey: String }
+
+  static targets = ["address"]
+
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    })
+    this.geocoder.addTo(this.element)
+  }
+
+  disconnect() {
+    this.geocoder.onRemove()
+}
+
+//import { Controller } from "@hotwired/stimulus"
+//import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="address-autocomplete"
+//export default class extends Controller {
+
+  }
+
